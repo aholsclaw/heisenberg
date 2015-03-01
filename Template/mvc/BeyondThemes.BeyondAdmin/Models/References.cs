84a747c4e0fe80aed0c6a97d4dfa0553d1898598
terriast <terri.ast@mylan.com>
2015-03-01 08:24:37 -0500

    Terri Changes

--- Template/mvc/BeyondThemes.BeyondAdmin/Models/References.cs
+++ Template/mvc/BeyondThemes.BeyondAdmin/Models/References.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net;
 using System.ServiceModel.Syndication;
-using System.Web;
-using System.Web.UI;
 using System.Xml;
 
 namespace BeyondThemes.BeyondAdmin.Models
